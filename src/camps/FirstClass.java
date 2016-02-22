95914f9c26f6d9060fac3bcf0f316b8c0c5a743f
Kovacsics Tamás <kovtomy@yahoo.com>
2016-02-22 10:50:40 +0100

    t1 finished

--- src/camps/FirstClass.java
+++ src/camps/FirstClass.java
@@ -1,18 +1,35 @@
 package camps;
 
+import org.openqa.selenium.WebElement;
+
 public class FirstClass {
 //we will write test metodes here
 	
 	/*test case T1
 	 * for now just open and close the web site 
 	 */
-	public static void t1()
+	public static void t1() throws InterruptedException
 	{
+		String title="Recreation Sites & Trail BC";
+		String text="Through Rec.SitesandTrails.BC we offer a respectful, family oriented wilderness camping experience, focused on improving all aspects of the customer’s enjoyment.";
+		
 		MainPage main=new MainPage();
+		for(WebElement button:main.getSmallButtons())
+		{
+			button.click();
+			Thread.sleep(2000);
+			if (main.slideTitle().compareTo(title)!=0)
+			{
+				System.err.println("Wrong title!\n title is:\n "+main.slideTitle());
+			}
+			if (main.slideText().compareTo(text)!=0)
+				System.err.println("Wrong text!\n text is:\n "+main.slideText());
+		}
+		Thread.sleep(2000);
 		main.close();
 	}
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		t1();
 	}
--- src/camps/MainPage.java
+++ src/camps/MainPage.java
@@ -1,6 +1,10 @@
 package camps;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MainPage {
@@ -18,5 +22,29 @@ public class MainPage {
 		driver.quit();
 	}
 	
+	public List<WebElement> getSmallButtons()
+	{
+		return driver.findElements(By.className("nivo-control"));
+	}
+	
+	public WebElement rightButton()
+	{
+		return driver.findElement(By.className("nivo-nextNav"));
+	}
+	
+	public WebElement leftButton()
+	{
+		return driver.findElement(By.className("nivo-prevNav"));
+	}
+	
+	public String slideTitle()
+	{
+		return driver.findElement(By.className("nivo-caption")).findElement(By.tagName("h2")).getText();
+	}
+	
+	public String slideText()
+	{
+		return driver.findElement(By.className("slide-text")).getText();
+	}
 	
 }
